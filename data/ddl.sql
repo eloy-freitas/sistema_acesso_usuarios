8dc67c3e9f22dda82dd757b8d26416a9a0139c66
eloy-freitas <freitaseloy.1@gmail.com>
2022-12-18 22:31:06 -0300

    feat: adicionando informações no usuario e criando tabelas  notificacao e notificacao_usuarios

--- data/ddl.sql
+++ data/ddl.sql
@@ -1,7 +1,28 @@
 create table usuario(
     id_usuario integer primary key autoincrement,
     nm_usuario varchar(100),
+    nm_login varchar(30),
     ds_senha varchar(50),
     ds_email varchar(150),
-    dt_modificacao datetime
+    fl_admin boolean,
+    fl_autorizado boolean,
+    dt_cadastro date,
+    dt_modificacao timestamp
 );
+
+create table notificacao(
+    id_notificacao integer primary key autoincrement,
+    id_remetente integer not null,
+    ds_mensagem varchar(300),
+    fl_lida boolean,
+    dt_envio timestamp,
+    FOREIGN KEY(id_remetente) REFERENCES usuario(id_usuario)
+);
+
+create table notificacao_usuarios(
+    id_notificacao integer not null,
+    id_destinatario integer not null,
+    FOREIGN KEY(id_destinatario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY(id_notificacao) REFERENCES notificacao(id_notificacao)
+);
+
