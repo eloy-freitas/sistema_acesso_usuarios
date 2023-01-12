7c780f6683da650eabe67f0df833910ad7a120ab
eloy-freitas <freitaseloy.1@gmail.com>
2023-01-11 23:55:38 -0300

    feat: interface de aviso de usuário não logado

--- src/main/java/com/ufes/sistemaacessousuarios/principalpresenter/state/LogadoState.java
+++ src/main/java/com/ufes/sistemaacessousuarios/principalpresenter/state/LogadoState.java
@@ -2,13 +2,18 @@ package com.ufes.sistemaacessousuarios.principalpresenter.state;
 
 import com.ufes.sistemaacessousuarios.model.Usuario;
 import com.ufes.sistemaacessousuarios.presenter.PrincipalPresenter;
+import com.ufes.sistemaacessousuarios.view.NaoAutorizadoView;
 
 
 public class LogadoState extends PrincipalPresenterState{
-
+    private Usuario usuario;
+    private NaoAutorizadoView naoAutorizadoView;
+    
     public LogadoState(PrincipalPresenter presenter) {
         super(presenter);
+        usuario = presenter.getUsuario(); 
         principalView = presenter.getPrincipalView();
+        naoAutorizadoView = new NaoAutorizadoView();
         initComponents();
     }
     
@@ -16,10 +21,15 @@ public class LogadoState extends PrincipalPresenterState{
     public void initComponents(){
         principalView.getMiLogin().setEnabled(false);
         principalView.getMiCadastrar().setEnabled(false);
-        Usuario usuario = presenter.getUsuario();
         principalView.getLblInfoUsuario().setText(criarInfoUsuario(usuario));
         principalView.getLblInfoUsuario().setVisible(true);
-        principalView.getBtnNotificacoes().setVisible(true);
+        
+        if(!usuario.isAutorizado()){
+            principalView.getBtnNotificacoes().setVisible(false);
+            presenter.getPrincipalView().getDpMenu().add(naoAutorizadoView);
+            naoAutorizadoView.setVisible(true);
+        }else
+            principalView.getBtnNotificacoes().setVisible(true);
     }
     
     private String criarInfoUsuario(Usuario usuario){
@@ -31,6 +41,8 @@ public class LogadoState extends PrincipalPresenterState{
     
     @Override
     public void sair(){
+        if(naoAutorizadoView.isVisible())
+            naoAutorizadoView.dispose();
         presenter.setEstado(new NaoLogadoState(presenter));
     }
     
